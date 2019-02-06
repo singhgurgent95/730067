12fbc9a1e107b5c60594a783d40291a965eb96bc
visualstudio <visualstudio@122-PC20>
2019-02-06 10:08:00 -0500

    week 5

--- 730067/Program.cs
+++ 730067/Program.cs
@@ -8,8 +8,30 @@ namespace _730067
 {
     class Program
     {
+        static int i = 0;
         static void Main(string[] args)
         {
+
+            MethodA();
+        }
+            public static void MethodA()
+            {
+                while (Peanut())
+                {
+                    Console.WriteLine(i);
+                if ( i>10)
+                    {
+                        return;
+                    }
+                }
+            }
+            public static bool Peanut()
+            {
+            i++;
+                return true;
+            }
         }
     }
-}
+
+
+
